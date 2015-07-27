56836fed4cf0aa51d4d60a1bb0a6aa460b8a1830
Ronak H Shah <rons.shah@gmail.com>
2015-07-27 18:35:43 -0400

    Updated CNV

--- PlotForCSFPaper/Pt64_cnv_cfDNA.R
+++ PlotForCSFPaper/Pt64_cnv_cfDNA.R
@@ -22,25 +22,27 @@ theme_mine <- function(base_size = 12, base_family = "") {
   # Starts with theme_grey and then modify some parts
   theme_bw(base_size = base_size, base_family = base_family) %+replace%
     theme(
-      axis.text.x = element_text(size=10,hjust=2,angle=0,face="bold"),
+      axis.text.x = element_text(size=14,hjust=2,angle=0,face="bold"),
       axis.text.y = element_text(size=14,,hjust = 1,face="bold"),
       axis.ticks.x =  element_blank(),
       axis.ticks.y =  element_blank(),
       axis.title.x =  element_blank(),
-      axis.title.y = element_blank(),
+      axis.title.y = element_text(size=16,angle=90,face="bold"),
       legend.position = "top",
       legend.direction="horizontal",
-      panel.grid.major.x = element_line(colour = "grey80", linetype = "dotted"),
+      panel.grid.major.x = element_line(colour = "grey", linetype = "dashed"),
       panel.grid.major.y = element_line(colour = "grey80")
     )
 }
 ggplot(Pt64_cfDNA,aes(x=seq(1,nrow(Pt64_cfDNA)),y=lr)) + theme_mine() +
   geom_jitter(aes(color = factor(sig))) + 
+  ylab("LOG2 Tumor/Normal Ratio") +
   scale_y_continuous(breaks=seq(-4,4,1),limits=c(-4,4)) + 
   scale_x_discrete(breaks=linepos,labels=seq(1,21,1)) + 
+  geom_hline(yintercept=0, linetype="dashed") +
   scale_color_manual(values=c("#0072B2","#D55E00"),
                      labels=c("Insignificant","Significant"),
                      guide=guide_legend(title = "", 
-                                        label.theme = element_text(angle=0,size=12,face="bold"))) 
+                                        label.theme = element_text(angle=0,size=18,face="bold"))) 
 
 ggsave("~/Documents/MSKCC/CMO/CSF_Analysis/Analysis_Plots/Figure_CNV_Pt64_cfDNA.pdf",width=20, height=5)
